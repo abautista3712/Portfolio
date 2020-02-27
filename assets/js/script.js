ef41d1416c183cfe317f66604f3d3d9148adb18f
Abelard Bautista <abelardbautista@gmail.com>
2020-02-26 23:28:54 -0800

    Added on-click function for Weather Now app.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,7 @@
 $("#inOrOut").on("click", function() {
   window.open("https://abautista3712.github.io/In-or-Out/", "_blank");
 });
+
+$("#weatherNow").on("click", function() {
+  window.open("https://abautista3712.github.io/WeatherDashboard/", "_blank");
+});
