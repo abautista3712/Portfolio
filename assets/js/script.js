d9c5af927d1c893853ac2c68d17b20a6cf039512
Abelard Bautista <abelardbautista@gmail.com>
2020-03-02 22:52:19 -0800

    Added live links to LinkedIn and GitHub profiles.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
+// On-click function for LinkedIn and GitHub
+$("#linkedInIcon").on("click", function() {
+  window.open("https://www.linkedin.com/in/abelard-bautista", "_blank");
+});
+
+$("#gitHubIcon").on("click", function() {
+  window.open("https://www.github.com/abautista3712", "_blank");
+});
+
+// Portfolio Projects
 $("#inOrOut").on("click", function() {
   window.open("https://abautista3712.github.io/In-or-Out/", "_blank");
 });
