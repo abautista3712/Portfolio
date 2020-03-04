fd327d82e6b52c3d24ac9f1c4b5dffbdc9bdb436
Abelard Bautista <abelardbautista@gmail.com>
2020-03-03 20:14:07 -0800

    Added on-hover event to each portfolio item.

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,15 +7,76 @@ $("#gitHubIcon").on("click", function() {
   window.open("https://www.github.com/abautista3712", "_blank");
 });
 
+// $("#bannerIOO").on("click", function() {
+//   $("#bannerIOO").text("");
+//   $("<div>", { id: "newDiv", dataAttribute: "isClicked" })
+//     .text("newDivCreated")
+//     .appendTo("#bannerIOO");
+// });
+
 // Portfolio Projects
-$("#inOrOut").on("click", function() {
-  window.open("https://abautista3712.github.io/In-or-Out/", "_blank");
-});
+// var resetBanner = () => {
+//   $(this).css("top", "auto");
+//   $(this).css("bottom", "10%");
+// };
 
-$("#weatherNow").on("click", function() {
-  window.open("https://abautista3712.github.io/WeatherDashboard/", "_blank");
-});
+// resetBanner();
 
-$("#dayPlanner").on("click", function() {
-  window.open("https://abautista3712.github.io/DayPlanner/", "_blank");
-});
+// function changeBannerPosition(order) {
+$("#inOrOut").hover(
+  function() {
+    $("#banner" + 1).css("top", "0%");
+    $("#banner" + 1).css("bottom", "auto");
+    console.log("hoverInSuccess");
+  },
+  function() {
+    // $("#inOrOut").attr("isHover", "false");
+    $("#banner" + 1).css("top", "auto");
+    $("#banner" + 1).css("bottom", "10%");
+    // $("#bannerIOO").css("top", null);
+    console.log("hoverOutSuccess");
+  }
+);
+
+$("#weatherNow").hover(
+  function() {
+    $("#banner" + 2).css("top", "0%");
+    $("#banner" + 2).css("bottom", "auto");
+    console.log("hoverInSuccess");
+  },
+  function() {
+    // $("#inOrOut").attr("isHover", "false");
+    $("#banner" + 2).css("top", "auto");
+    $("#banner" + 2).css("bottom", "10%");
+    // $("#bannerIOO").css("top", null);
+    console.log("hoverOutSuccess");
+  }
+);
+
+$("#dayPlanner").hover(
+  function() {
+    $("#banner" + 3).css("top", "0%");
+    $("#banner" + 3).css("bottom", "auto");
+    console.log("hoverInSuccess");
+  },
+  function() {
+    // $("#inOrOut").attr("isHover", "false");
+    $("#banner" + 3).css("top", "auto");
+    $("#banner" + 3).css("bottom", "10%");
+    // $("#bannerIOO").css("top", null);
+    console.log("hoverOutSuccess");
+  }
+);
+
+// }
+
+// for (a = 1; a < 4; a++) {
+//   changeBannerPosition(a);
+// }
+// $("#weatherNow").on("click", function() {
+//   window.open("https://abautista3712.github.io/WeatherDashboard/", "_blank");
+// });
+
+// $("#dayPlanner").on("click", function() {
+//   window.open("https://abautista3712.github.io/DayPlanner/", "_blank");
+// });
