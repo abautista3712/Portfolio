d55daa373b4ca7722b67ad98b36c92591271d794
Abelard Bautista <abelardbautista@gmail.com>
2020-03-05 12:28:49 -0800

    Added divs to link demo and GitHub repo for each project.

--- assets/js/script.js
+++ assets/js/script.js
@@ -30,7 +30,7 @@ $("#inOrOut").hover(
     $("#banner" + 1).css("z-index", "1");
     $("<div>", { id: "descriptionBackground" + 1, class: "flexCenter" })
       .css("padding", "0px 10%")
-      .css("height", "100%")
+      .css("height", "80%")
       .css("background", "rgba(0,0,0,0.8)")
       .css("color", "#fff")
       .text(
@@ -38,12 +38,28 @@ $("#inOrOut").hover(
       )
       .appendTo("#inOrOut");
     $("#descriptionBackground" + 1);
+    $("<div>", { id: "demoLink" + 1, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("Demo")
+      .appendTo("#inOrOut");
+    $("<div>", { id: "gitHubRepo" + 1, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("GitHub Repository")
+      .appendTo("#inOrOut");
     console.log("hoverInSuccess");
   },
   function() {
     $("#banner" + 1).css("top", "auto");
     $("#banner" + 1).css("bottom", "10%");
     $("#descriptionBackground" + 1).remove();
+    $("#demoLink" + 1).remove();
+    $("#gitHubRepo" + 1).remove();
     console.log("hoverOutSuccess");
   }
 );
@@ -55,7 +71,7 @@ $("#weatherNow").hover(
     $("#banner" + 2).css("z-index", "1");
     $("<div>", { id: "descriptionBackground" + 2, class: "flexCenter" })
       .css("padding", "0px 10%")
-      .css("height", "100%")
+      .css("height", "80%")
       .css("background", "rgba(0,0,0,0.8)")
       .css("color", "#fff")
       .text(
@@ -63,12 +79,28 @@ $("#weatherNow").hover(
       )
       .appendTo("#weatherNow");
     $("#descriptionBackground" + 2);
+    $("<div>", { id: "demoLink" + 2, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("Demo")
+      .appendTo("#weatherNow");
+    $("<div>", { id: "gitHubRepo" + 2, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("GitHub Repository")
+      .appendTo("#weatherNow");
     console.log("hoverInSuccess");
   },
   function() {
     $("#banner" + 2).css("top", "auto");
     $("#banner" + 2).css("bottom", "10%");
     $("#descriptionBackground" + 2).remove();
+    $("#demoLink" + 2).remove();
+    $("#gitHubRepo" + 2).remove();
     console.log("hoverOutSuccess");
   }
 );
@@ -80,7 +112,7 @@ $("#dayPlanner").hover(
     $("#banner" + 3).css("z-index", "1");
     $("<div>", { id: "descriptionBackground" + 3, class: "flexCenter" })
       .css("padding", "0px 10%")
-      .css("height", "100%")
+      .css("height", "80%")
       .css("background", "rgba(0,0,0,0.8)")
       .css("color", "#fff")
       .text(
@@ -88,15 +120,81 @@ $("#dayPlanner").hover(
       )
       .appendTo("#dayPlanner");
     $("#descriptionBackground" + 3);
+    $("<div>", { id: "demoLink" + 3, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("Demo")
+      .appendTo("#dayPlanner");
+    $("<div>", { id: "gitHubRepo" + 3, class: "flexCenter" })
+      .css("padding", "0px 10%")
+      .css("height", "10%")
+      .css("background", "rgba(0,0,0,0.8)")
+      .css("color", "#fff")
+      .text("GitHub Repository")
+      .appendTo("#dayPlanner");
     console.log("hoverInSuccess");
   },
   function() {
     $("#banner" + 3).css("top", "auto");
     $("#banner" + 3).css("bottom", "10%");
     $("#descriptionBackground" + 3).remove();
+    $("#demoLink" + 3).remove();
+    $("#gitHubRepo" + 3).remove();
     console.log("hoverOutSuccess");
   }
 );
+
+// $("#weatherNow").hover(
+//   function() {
+//     $("#banner" + 2).css("top", "0%");
+//     $("#banner" + 2).css("bottom", "auto");
+//     $("#banner" + 2).css("z-index", "1");
+//     $("<div>", { id: "descriptionBackground" + 2, class: "flexCenter" })
+//       .css("padding", "0px 10%")
+//       .css("height", "100%")
+//       .css("background", "rgba(0,0,0,0.8)")
+//       .css("color", "#fff")
+//       .text(
+//         "A web application that provides the weather conditions for a user-chosen city."
+//       )
+//       .appendTo("#weatherNow");
+//     $("#descriptionBackground" + 2);
+//     console.log("hoverInSuccess");
+//   },
+//   function() {
+//     $("#banner" + 2).css("top", "auto");
+//     $("#banner" + 2).css("bottom", "10%");
+//     $("#descriptionBackground" + 2).remove();
+//     console.log("hoverOutSuccess");
+//   }
+// );
+
+// $("#dayPlanner").hover(
+//   function() {
+//     $("#banner" + 3).css("top", "0%");
+//     $("#banner" + 3).css("bottom", "auto");
+//     $("#banner" + 3).css("z-index", "1");
+//     $("<div>", { id: "descriptionBackground" + 3, class: "flexCenter" })
+//       .css("padding", "0px 10%")
+//       .css("height", "100%")
+//       .css("background", "rgba(0,0,0,0.8)")
+//       .css("color", "#fff")
+//       .text(
+//         "An editable web application capable of recording the user's schedule by the hour."
+//       )
+//       .appendTo("#dayPlanner");
+//     $("#descriptionBackground" + 3);
+//     console.log("hoverInSuccess");
+//   },
+//   function() {
+//     $("#banner" + 3).css("top", "auto");
+//     $("#banner" + 3).css("bottom", "10%");
+//     $("#descriptionBackground" + 3).remove();
+//     console.log("hoverOutSuccess");
+//   }
+// );
 
 // }
 
